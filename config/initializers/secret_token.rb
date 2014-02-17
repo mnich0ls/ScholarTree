23c1a214afd0e1ce003b72f6aca6381411210b27
Mike Nichols <mikenichols2@icloud.com>
2014-02-16 17:43:08 -0800

    Initial commit. Welcome to the world, ScholarTree.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScholarTree::Application.config.secret_key_base = '283340263e9ad4170ad7e17751e0a8f9e3c017458de9f77517a3a2a388e683fc44aee7bb6d06a539fb44183cf0988fc62ae53586c692cf2fdc06802fdd23bb24'
